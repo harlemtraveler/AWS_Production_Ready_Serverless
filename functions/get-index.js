da45ec42d096bad802505536fa3a029f38de35ff
Timothy <timothy.keaveny@troyweb.com>
2019-10-04 17:31:12 -0400

    Add "index.html" && Modified "get-index" function to return the HTML file when invoked

--- functions/get-index.js
+++ functions/get-index.js
@@ -1,14 +1,29 @@
 'use strict';
 
-module.exports.handler = async (event) => {
-  return {
+const co = require("co");
+const Promise = require("bluebird");
+const fs = Promise.promisifyAll(require("fs"));
+
+var html;
+
+function* loadHtml() {
+  if (!html) {
+    html = yield fs.readFileAsync('static/index.html', 'utf-8');
+  }
+  return html;
+}
+
+module.exports.handler = co.wrap(function* (event, context, callback) {
+  let html = yield loadHtml();
+  const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+    body: html,
+    headers: {
+      'Content-Type': 'text/html; charset=UTF-8',
+    }
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  // This line returns the html file in a callback - IMPORTANT!!!
+  callback(null, response);
+
+});
