7ecf3433f5a0fe883c4fea457a46342ba7725ea8
Timothy <timothy.keaveny@troyweb.com>
2019-10-08 14:04:07 -0400

    Update app to have "get-index" interact with the "get-restaurants" endpoint as an Internal API

--- functions/get-index.js
+++ functions/get-index.js
@@ -3,6 +3,12 @@
 const co = require("co");
 const Promise = require("bluebird");
 const fs = Promise.promisifyAll(require("fs"));
+const Mustache = require("mustache");
+// The "superagent" & "superagent-promise" libraries are used to call the "get-restaurants" endpoint and handle callbacks
+const http = require("superagent-promise")(require("superagent"), Promise);
+
+const restaurantsApiRoot = process.env.restaurants_api;
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 var html;
 
@@ -13,8 +19,16 @@ function* loadHtml() {
   return html;
 }
 
+function* getRestaurants() {
+  return (yield http.get(restaurantsApiRoot)).body; // The "restaurantsApiRoot" Env variable is used to store the "get-restaurants" endpoint URL
+}
+
 module.exports.handler = co.wrap(function* (event, context, callback) {
-  let html = yield loadHtml();
+  let template = yield loadHtml(); // loads HTML as a template
+  let restaurants = yield getRestaurants(); // getRestaurants() is tentative
+  let dayOfWeek = days[new Date().getDay()]; // 
+  let html = Mustache.render(template, { dayOfWeek, restaurants }); // loads template, passing restaurants as params
+
   const response = {
     statusCode: 200,
     body: html,
