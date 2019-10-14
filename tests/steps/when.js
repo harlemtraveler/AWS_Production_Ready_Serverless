ff11cd5ccfb131b9749012623d8d08d062b2d292
Timothy <timothy.keaveny@troyweb.com>
2019-10-14 00:38:12 -0400

    Refactor code to match course refactor

--- tests/steps/when.js
+++ tests/steps/when.js
@@ -1,44 +1,43 @@
 'use strict';
 
 const APP_ROOT = '../../';
+const _ = require('lodash');
 
-/*
-  The "when" step inokes the "get-index" function with a "stubbed" ( an empty object -> {} ) event and context.
-*/
+function viaHandler(event, functionName) {
+  let handler = require(`${APP_ROOT}/functions/${functionName}`).handler;
 
-let we_invoke_get_index = function() {
-  // Load the handler function for "get-index":
-  let handler = require(`${APP_ROOT}/functions/get-index`).handler;
-
-  // Return a new promise:
   return new Promise((resolve, reject) => {
-    // "Stub" the "context" we're going to call the handler function with:
     let context = {};
-    // resolve the promise when handler func calls the cb we're going to pass in:
-    let callback = function(err, response) {
+    let callback = function (err, response) {
       if (err) {
-        // If an error is returned, reject the promise:
         reject(err);
       } else {
-        // respond with HTTP repsonse, using lodash to obtain the Content-Type:
         let contentType = _.get(response, 'headers.Content-Type', 'application/json');
-        // If Content-Type doesn't exist, default to "application/json":
         if (response.body && contentType === 'application/json') {
-          // If Content-Type is "application/json", just pass the JSON body:
           response.body = JSON.parse(response.body);
         }
 
-        // Resolve the promise with the HTTP response:
-        response(response);
+        resolve(response);
       }
     };
 
-    // Call the handler function with a "stubbed" event object as well:
-    handler({}, context, callback);
+    handler(event, context, callback);
   });
-};
+}
 
-// Export the function:
+let we_invoke_get_index = () => viaHandler({}, 'get-index');
+
+let we_invoke_get_restaurants = () => viaHandler({}, 'get-restaurants');
+
+let we_invoke_search_restaurants = theme => {
+  let event = { 
+    body: JSON.stringify({ theme })
+  };
+  return viaHandler(event, 'search-restaurants');
+}
+
 module.exports = {
-  we_invoke_get_index
-};
+  we_invoke_get_index,
+  we_invoke_get_restaurants,
+  we_invoke_search_restaurants
+}
