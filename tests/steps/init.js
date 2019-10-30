bbfaea7363d4ce7ad60e8e72662fe0ae9e308de2
Timothy <timothy.keaveny@troyweb.com>
2019-10-30 17:48:26 -0400

    Add logic to retrieve the acct credential for the "Local" defualt user

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -16,14 +16,26 @@ let init = co.wrap(function* () {
   process.env.restaurants_api = "https://ozk1g4x3w2.execute-api.us-east-1.amazonaws.com/dev/restaurants";
   process.env.restaurants_table = "restaurants";
   process.env.AWS_REGION = "us-east-1";
-  process.env.cognito_user_pool_id = "cognito_user_pool_id"; // dummy value
+  process.env.cognito_user_pool_id = "us-east-1_CeZvkfvM4";
   process.env.cognito_client_id = "cognito_client_id"; // dummy value
+  process.env.cognito_server_client_id = "cognito_server_client_id";
+
+  if (!process.env.AWS_ACCESS_KEY_ID) {
+    let cred = (yield awscred.loadAsync()).credentials;
+
+    process.env.AWS_ACCESS_KEY_ID = cred.accessKeyId;
+    process.env.AWS_SECRET_ACCESS_KEY = cred.secretAccessKey;
+
+    if (cred.sessionToken) {
+      process.env.AWS_SESSION_TOKEN = cred.sessionToken;
+    }
+  }
 
   // The "bluebird" pkg added the "loadAsync()" function to "awscred"
-  let cred = (yield awscred.loadAsync()).credentials;
+  // let cred = (yield awscred.loadAsync()).credentials;
 
-  process.env.AWS_ACCESS_KEY_ID = cred.accessKeyId;
-  process.env.AWS_SECRET_ACCESS_KEY = cred.secretAccessKey;
+  // process.env.AWS_ACCESS_KEY_ID = cred.accessKeyId;
+  // process.env.AWS_SECRET_ACCESS_KEY = cred.secretAccessKey;
 
   console.log("AWS credentials loaded");
 
