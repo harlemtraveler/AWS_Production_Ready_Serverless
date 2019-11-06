1d5f17d8fe0e1043d1f05bd1ecb505380683553f
Timothy <timothy.keaveny@troyweb.com>
2019-11-06 15:12:39 -0500

    Comment-out a console log cmd for testing purposes

--- tests/test_cases/get-index.js
+++ tests/test_cases/get-index.js
@@ -21,7 +21,7 @@ describe(`When we invoke the GET / endpoint`, co.wrap(function* () {
     expect(res.headers['content-type']).to.equal('text/html; charset=UTF-8'); // Changed "UTF-8" to "utf-8"
     expect(res.body).to.not.be.null;
 
-    console.log(res.body);
+    // console.log(res.body);
 
     let $ = cheerio.load(res.body);
     let restaurants = $('.restaurant', '#restaurantsUl'); // "restaurantsUl" = restaurants unsorted list
